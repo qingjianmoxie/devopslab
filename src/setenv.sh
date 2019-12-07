db37513d629b6b61da1842ad95241b4669fb0940
lizeyang <610556220@qq.com>
2019-12-07 13:36:43 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=zeyangli
 
 export MYCLUSTER=tektonknativebeijing32
-export KUBECONFIG=
+export KUBECONFIG=tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing32/kube-config-tok02-tektonknativebeijing32.yml
 
 export NAMESPACE=zeyangli
 export EMAIL=610556220@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing32-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
